4bf0b1adac9caa6697794e6c149685c133818159
David <example@gmail.com>
2023-06-28 15:52:59 +0300

    fixed js script files

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
    $('.carousel__inner').slick({
       speed: 1200,
       // adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="./icons/leftArrow.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="./icons/rightArrow.png"></button>',
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/leftArrow.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/rightArrow.png"></button>',
       responsive: [
          {
             breakpoint: 992,
