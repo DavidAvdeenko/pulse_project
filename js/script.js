191f42dc31a35cf6a4b83c0d55677f93fe9ec1a1
David <example@gmail.com>
2023-06-28 14:53:45 +0300

    fourth commit, added slick-slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 $(document).ready(function () {
-   $('.carousel__inner').slick();
+   $('.carousel__inner').slick({
+      speed: 1200,
+      // adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="./icons/leftArrow.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="./icons/rightArrow.png"></button>',
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               dots: true,
+               arrows: false,
+            }
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2
+            }
+         },
+         {
+            breakpoint: 480,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+            }
+         }
+      ]
+   });
 });
